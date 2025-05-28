35901b90e311295b09a25ef55ed4d8135045337f
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-05-28 11:16:24 +0530

    Added Joins Excercise

--- 9.One_To_Many_And_Joins.sql
+++ 9.One_To_Many_And_Joins.sql
@@ -124,6 +124,80 @@ VALUES ('2016-02-10', 99.99, 1),
 			REFERENCES customers (id)
 			ON DELETE CASCADE
 		);
+        
+-- JOIN EXERCISE
+
+		CREATE TABLE students
+        (
+		   id INT PRIMARY KEY AUTO_INCREMENT,
+           first_name VARCHAR(20)
+        );
+
+        CREATE TABLE papers
+        (
+			student_id INT,
+			title VARCHAR(100),
+            grade INT,
+            FOREIGN KEY(student_id)
+				REFERENCES students(id)
+        );
+        
+        DROP TABLE papers;
+        DROP TABLE students;
+        
+        INSERT INTO students (first_name) VALUES 
+		('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
+		 
+		INSERT INTO papers (student_id, title, grade ) VALUES
+		(1, 'My First Book Report', 60),
+		(1, 'My Second Book Report', 75),
+		(2, 'Russian Lit Through The Ages', 94),
+		(2, 'De Montaigne and The Art of The Essay', 98),
+		(4, 'Borges and Magical Realism', 89);
 		
+        SELECT * FROM students;
+        SELECT * FROM papers;
+        
+        SELECT first_name ,title,grade
+        FROM students
+        JOIN papers
+        ON students.id = papers.student_id;
+        
+		SELECT first_name ,title,grade
+        FROM students
+        LEFT JOIN papers
+        ON students.id = papers.student_id;
+        
+        SELECT first_name,IFNULL(title,'MISSING'),IFNULL(grade,0)
+        FROM students
+        LEFT JOIN papers
+        ON students.id = papers.student_id;
+        
+        
+        SELECT 
+			first_name, IFNULL(AVG(grade), 0) AS average
+		FROM
+			students
+        LEFT JOIN
+			papers ON students.id = papers.student_id
+		GROUP BY first_name
+		ORDER BY average DESC;
+        
+        SELECT 
+			first_name,
+		IFNULL(AVG(grade), 0) AS average,
+			CASE
+				WHEN IFNULL(AVG(grade), 0) >= 75 THEN 'passing'
+				ELSE 'failing'
+			END AS passing_status
+		FROM
+			students
+        LEFT JOIN
+			papers ON students.id = papers.student_id
+		GROUP BY first_name
+		ORDER BY average DESC;
+        
+				
+			
     
        
