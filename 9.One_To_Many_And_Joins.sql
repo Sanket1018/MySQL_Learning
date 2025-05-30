69b8dc14a0ca03a9d29854f3cfa9bdf9e3d4d435
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-05-30 11:53:21 +0530

    Adding challenge #7

--- 9.One_To_Many_And_Joins.sql
+++ 9.One_To_Many_And_Joins.sql
@@ -387,7 +387,44 @@ FROM
     reviews ON reviewers.id = reviews.reviewer_id
 GROUP BY first_name , last_name;
 
+-- Challenge #7
 
+SELECT 
+    title,
+    rating,
+    CONCAT(first_name, ' ', last_name) AS reviewer
+FROM
+    reviews
+        INNER JOIN
+    series ON reviews.series_id = series.id
+        INNER JOIN
+    reviewers ON reviews.reviewer_id = reviewers.id;
+ 
+ 
+ 
+SELECT 
+    title,
+    rating,
+    CONCAT(first_name, ' ', last_name) AS reviewer
+FROM
+    series
+        INNER JOIN
+    reviews ON reviews.series_id = series.id
+        INNER JOIN
+    reviewers ON reviews.reviewer_id = reviewers.id;
+ 
+ 
+ 
+SELECT 
+    title,
+    rating,
+    CONCAT(first_name, ' ', last_name) AS reviewer
+FROM
+    reviewers
+        INNER JOIN
+    reviews ON reviews.reviewer_id = reviewers.id
+        INNER JOIN
+    series ON reviews.series_id = series.id;
 
     
     
